370a6dff26d10970b7ef1f7df444906a8368a234
zameerSheikh <zameer@gmail.com>
2019-09-18 13:20:34 +0530

    serving html files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/info', (req, res) => {
-    res.send(`Hi I am zameer sheikh`)
+    res.sendFile(`views/info.html`)
 });
 
 app.use(function (req, res, next) {
